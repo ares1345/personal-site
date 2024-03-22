2e5d96a2348bbc19c06ccf942264dc3fc91d6c34
ares1345 <ares1345istaken@gmail.com>
2024-03-22 22:29:11 +0100

    lots of changes

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger-menu').addEventListener('click', function() {
+    document.querySelector('.container').classList.toggle('inactive');
+    document.getElementById('menu').classList.toggle('active');
+});
