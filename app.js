59d94b80d130876d4aaa32b1db81cba3713cb012
ares1345 <ares1345istaken@gmail.com>
2024-03-22 23:02:58 +0100

    some more changes

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
+const button = document.getElementById('hamburger-menu');
+
+button.classList.toggle('active');
+
 document.getElementById('hamburger-menu').addEventListener('click', function() {
     document.querySelector('.container').classList.toggle('inactive');
     document.getElementById('menu').classList.toggle('active');
-});
+    if (button.classList.contains('active')) {
+        button.innerText = 'Close';
+        button.classList.remove('active');
+    } else {
+        button.innerText = 'Projets ☰';
+        button.classList.add('active');
+}});
+
+
+
+const menu = document.getElementById("menu");
+
+Array.from(document.getElementsByClassName("menu-item"))
+  .forEach((item, index) => {
+    item.onmouseover = () => {
+      menu.dataset.activeIndex = index;
+    }
+  });
